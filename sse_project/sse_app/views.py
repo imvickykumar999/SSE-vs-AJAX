c4637a203473bf300f04ed22c72408afe5b7cc6e
Vicky Kumar <imvickykumar999@gmail.com>
2025-03-07 09:57:58 +0000

    added Docstrings

--- sse_project/sse_app/views.py
+++ sse_project/sse_app/views.py
@@ -3,13 +3,13 @@ from django.http import StreamingHttpResponse
 from django.shortcuts import render
 
 def event_stream():
-    # """Generator function to send data to client"""
+    """Generator function to send data to client"""
     while True:
         yield f"data: {time.strftime('%H:%M:%S')}\n\n"
         time.sleep(1) 
 
 def sse_view(request):
-    # """Django view to handle SSE"""
+    """Django view to handle SSE"""
     response = StreamingHttpResponse(
         event_stream(), 
         content_type='text/event-stream'
